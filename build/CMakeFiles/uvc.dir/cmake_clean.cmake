7d72576f6e917d491c5f7d7f3835fdffe464b81d
Prashant Kumar <prash.k20@gmail.com>
2018-05-10 19:04:05 -0400

    added Mavlink gps & imu logging and TX2 GPIO trigger

---
+++ build/CMakeFiles/uvc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uvc.dir/uvc_cam.cpp.o"
+  "libuvc.pdb"
+  "libuvc.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uvc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
